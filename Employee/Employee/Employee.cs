6dc6b9dc76c114c8066558d789975a68ef80b8fa
mxbArt <berezovskiy.96@bk.ru>
2016-07-25 13:07:07 +0300

    employee

---
+++ Employee/Employee/Employee.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Employee : IComparable<Employee>
+    {
+        private string _name;
+        private decimal _salary;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (!value.Equals(""))
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return _salary;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _salary = value;
+                }
+                else
+                {
+                    Console.WriteLine("Salary is out of range. Salary must be more then 0");
+                }
+            }
+        }
+
+        private Employee() { }
+
+        public Employee(string newName, decimal newSalary)
+        {
+            _name = newName;
+            if (newSalary > 0)
+            {
+                _salary = newSalary;
+            }
+        }
+
+        public void Write()
+        {
+            Console.WriteLine("Name: " + _name + ", Salary: " + _salary);
+        }
+
+        public int CompareTo(Employee compareEmp)
+        {
+            if (compareEmp == null)
+            {
+                return 1;
+            }
+            return _salary.CompareTo(compareEmp._salary); 
+        }
+    }
+
+    class EpmComparer : IComparer<Employee>
+    {
+        public int Compare(Employee x, Employee y)
+        {
+            if (x.Name == null && y.Name == null) return 0;
+            else if (x.Name == null) return -1;
+            else if (y.Name == null) return 1;
+            else return x.Name.CompareTo(y.Name);
+        }
+    }
+}
---
+++ Employee/Employee/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>();
+            list.Add(new Employee("XXX", 2100));
+            list.Add(new Employee("Andrew", 1002));
+            list.Add(new Employee("Bravo", 200));
+            foreach (Employee emp in list)
+            {
+                emp.Write();
+            }
+            list.Sort();
+            Console.WriteLine("After sorting by salary");
+            foreach (Employee emp in list)
+            {
+                emp.Write();
+            }
+            EpmComparer empComp= new EpmComparer();
+            list.Sort(empComp);
+            Console.WriteLine("After sorting by name:");
+            foreach (Employee emp in list)
+            {
+                emp.Write();
+            }
+        }
+    }
+}
